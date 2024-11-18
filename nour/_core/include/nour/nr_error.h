8eb022fefeef7fd3059b2daa506f5d264690b7b2
= <=>
2024-11-18 16:07:10 +0300

    merged all methods to nr_error.h

--- nour/_core/include/nour/nr_error.h
+++ nour/_core/include/nour/nr_error.h
@@ -4,6 +4,8 @@
 #define NERROR_MAX_STRING_LEN 256
 
 #include "nr_config.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 typedef enum {
     NError_NoError,
@@ -26,5 +28,138 @@ typedef struct
     NError_Type type;
     char context[NERROR_MAX_STRING_LEN];
 }NError;
+
+static NError __NR_NERROR_GLOBAL_ERROR_VAR__= {
+    .type = NError_NoError,
+    .context = ""
+};
+
+NR_PRIVATE void
+_NError_TypeAsString(NError_Type type, char* trg_str) {
+    switch (type) {
+        case NError_NoError:
+            strcpy(trg_str, "NoError");
+            break;
+        case NError_MemoryError:
+            strcpy(trg_str, "MemoryError");
+            break;
+        case NError_TypeError:
+            strcpy(trg_str, "TypeError");
+            break;
+        case NError_IndexError:
+            strcpy(trg_str, "IndexError");
+            break;
+        case NError_ValueError:
+            strcpy(trg_str, "ValueError");
+            break;
+        case NError_IOError:
+            strcpy(trg_str, "IOError");
+            break;
+        case NError_ZeroDivisionError:
+            strcpy(trg_str, "ZeroDivisionError");
+            break;
+        case NError_ImportError:
+            strcpy(trg_str, "ImportError");
+            break;
+        case NError_AttributeError:
+            strcpy(trg_str, "AttributeError");
+            break;
+        case NError_KeyError:
+            strcpy(trg_str, "KeyError");
+            break;
+        case NError_AssertionError:
+            strcpy(trg_str, "AssertionError");
+            break;
+        case NError_RuntimeError:
+            strcpy(trg_str, "RuntimeError");
+            break;
+        case NError_OverflowError:
+            strcpy(trg_str, "OverflowError");
+            break;
+        default:
+            strcpy(trg_str, "UnknownError");
+            break;
+    }
+}
+
+NR_HEADER int 
+NError_IsError(){
+    return __NR_NERROR_GLOBAL_ERROR_VAR__.type != NError_NoError;
+}
+
+NR_HEADER void* 
+NError_RaiseError(NError_Type type, const char *format, ...) {
+    __NR_NERROR_GLOBAL_ERROR_VAR__.type = type;
+
+    va_list args;
+    va_start(args, format);
+
+    vsnprintf(__NR_NERROR_GLOBAL_ERROR_VAR__.context, NERROR_MAX_STRING_LEN, format, args);
+
+    va_end(args);
+
+    return NULL;
+}
+
+NR_HEADER void* 
+NError_RaiseErrorNoContext(NError_Type type){
+    __NR_NERROR_GLOBAL_ERROR_VAR__.type = type;
+    return NULL;
+}
+
+NR_HEADER void 
+NError_Print(){
+    char type_str[30];
+    _NError_TypeAsString(__NR_NERROR_GLOBAL_ERROR_VAR__.type, type_str);
+    printf("%s: %s\n", type_str, __NR_NERROR_GLOBAL_ERROR_VAR__.context);
+}
+
+NR_HEADER void* NError_RaiseMemoryError() {
+    return NError_RaiseErrorNoContext(NError_MemoryError);
+}
+
+NR_HEADER void* NError_RaiseTypeError() {
+    return NError_RaiseErrorNoContext(NError_TypeError);
+}
+
+NR_HEADER void* NError_RaiseIndexError() {
+    return NError_RaiseErrorNoContext(NError_IndexError);
+}
+
+NR_HEADER void* NError_RaiseValueError() {
+    return NError_RaiseErrorNoContext(NError_ValueError);
+}
+
+NR_HEADER void* NError_RaiseIOError() {
+    return NError_RaiseErrorNoContext(NError_IOError);
+}
+
+NR_HEADER void* NError_RaiseZeroDivisionError() {
+    return NError_RaiseErrorNoContext(NError_ZeroDivisionError);
+}
+
+NR_HEADER void* NError_RaiseImportError() {
+    return NError_RaiseErrorNoContext(NError_ImportError);
+}
+
+NR_HEADER void* NError_RaiseAttributeError() {
+    return NError_RaiseErrorNoContext(NError_AttributeError);
+}
+
+NR_HEADER void* NError_RaiseKeyError() {
+    return NError_RaiseErrorNoContext(NError_KeyError);
+}
+
+NR_HEADER void* NError_RaiseAssertionError() {
+    return NError_RaiseErrorNoContext(NError_AssertionError);
+}
+
+NR_HEADER void* NError_RaiseRuntimeError() {
+    return NError_RaiseErrorNoContext(NError_RuntimeError);
+}
+
+NR_HEADER void* NError_RaiseOverflowError() {
+    return NError_RaiseErrorNoContext(NError_OverflowError);
+}
 
 #endif
