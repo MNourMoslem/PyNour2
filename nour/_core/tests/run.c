44b51b009b5ea542cd5d1605998463579b936c74
= <=>
2024-11-24 11:45:06 +0300

    included test_tc_methods.h and added its test function to tests

--- nour/_core/tests/run.c
+++ nour/_core/tests/run.c
@@ -1,10 +1,12 @@
 #include "node_creation.h"
 #include "test_nmath.h"
+#include "test_tc_methods.h"
 #include "niters.h"
 
 int main(){
     test_node_creation();
     test_niters();
     test_nmath();
+    test_tc_methods();
     return 0;
 }
