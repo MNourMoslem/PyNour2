2d80aaef5cd363fd1237b6471beac7743ceae4c0
= <=>
2024-11-24 11:39:19 +0300

    created nr_math.h and added bacis math opetaions

---
+++ nour/_core/include/nour/nr_math.h
@@ -0,0 +1,20 @@
+#ifndef NOUR__CORE_INCLUDE__NOUR_NR_MATH_H
+#define NOUR__CORE_INCLUDE__NOUR_NR_MATH_H
+
+#include <math.h>
+
+#define NMATH_ADD(a, b) (a + b)
+#define NMATH_MUL(a, b) (a * b)
+#define NMATH_SUB(a, b) (a - b)
+#define NMATH_DIV(a, b) (a / b)
+#define NMATH_POW(a, b) (pow(a, b))
+#define NMATH_MOD(a, b) (a % b)
+#define NMATH_TDV(a, b) (a / b)
+#define NMATH_BG(a, b) (a > b)
+#define NMATH_BGE(a, b) (a >= b)
+#define NMATH_LS(a, b) (a < b)
+#define NMATH_LSE(a, b) (a <= b)
+#define NMATH_EQ(a, b) (a == b)
+#define NMATH_NEQ(a, b) (a != b)
+
+#endif
