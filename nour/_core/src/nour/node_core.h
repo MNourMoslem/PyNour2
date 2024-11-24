fc75f3913177e3be14a08218bb10110757456914
= <=>
2024-11-24 11:49:41 +0300

    added Node_NewEmpty and Node_Copy functions

--- nour/_core/src/nour/node_core.h
+++ nour/_core/src/nour/node_core.h
@@ -2,8 +2,15 @@
 #define NOUR__CORE_SRC_NOUR_NODE_CORE_H
 
 #include "nour/nour.h"
+#include "tc_methods.h"
 
 NR_PUBLIC Node*
 Node_New(void* data, int copy_data, int ndim, nr_size_t* shape, NR_DTYPE dtype);
+
+NR_PUBLIC Node*
+Node_NewEmpty(int ndim, nr_size_t* shape, NR_DTYPE dtype);
+
+NR_PUBLIC Node*
+Node_Copy(Node* dst, const Node* src);
 
 #endif
