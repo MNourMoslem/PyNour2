d6dbd9e42bbc7fd2b91f2d4192a5e7463a6aa267
= <=>
2024-11-24 11:37:49 +0300

    add dtype to string methods and add NR_NUM_NUMIRC_DT to NR_DTYPE enum

--- nour/_core/include/nour/nr_dtypes.h
+++ nour/_core/include/nour/nr_dtypes.h
@@ -14,7 +14,8 @@ typedef enum{
     NR_INT64,
     NR_UINT64,
     NR_FLOAT32,
-    NR_FLOAT64
+    NR_FLOAT64,
+    NR_NUM_NUMIRC_DT
 }NR_DTYPE;
 
 typedef struct
@@ -41,5 +42,135 @@ NR_HEADER nr_size_t
 NDtype_Size(NR_DTYPE dtype){
     return __NR_NDTYPE_SIZES__[dtype];
 }
+
+NR_HEADER void
+NDtype_AsString(NR_DTYPE dtype, char dst[]){
+    switch (dtype)
+    {
+    case NR_BOOL:
+        strcpy(dst, "NR_BOOL");
+        break;
+    case NR_INT8:
+        strcpy(dst, "NR_INT8");
+        break;
+    case NR_UINT8:
+        strcpy(dst, "NR_UINT8");
+        break;
+    case NR_INT16:
+        strcpy(dst, "NR_INT16");
+        break;
+    case NR_UINT16:
+        strcpy(dst, "NR_UINT16");
+        break;
+    case NR_INT32:
+        strcpy(dst, "NR_INT32");
+        break;
+    case NR_UINT32:
+        strcpy(dst, "NR_UINT32");
+        break;
+    case NR_INT64:
+        strcpy(dst, "NR_INT64");
+        break;
+    case NR_UINT64:
+        strcpy(dst, "NR_UINT64");
+        break;
+    case NR_FLOAT32:
+        strcpy(dst, "NR_FLOAT32");
+        break;
+    case NR_FLOAT64:
+        strcpy(dst, "NR_FLOAT64");
+        break;
+    default:
+        strcpy(dst, "UNKNOWN");
+        break;
+    }
+}
+
+NR_HEADER void
+NDtype_AsStringVarType(NR_DTYPE dtype, char dst[]){
+    switch (dtype)
+    {
+    case NR_BOOL:
+        strcpy(dst, "nr_bool");
+        break;
+    case NR_INT8:
+        strcpy(dst, "nr_int8");
+        break;
+    case NR_UINT8:
+        strcpy(dst, "nr_uint8");
+        break;
+    case NR_INT16:
+        strcpy(dst, "nr_int16");
+        break;
+    case NR_UINT16:
+        strcpy(dst, "nr_uint16");
+        break;
+    case NR_INT32:
+        strcpy(dst, "nr_int32");
+        break;
+    case NR_UINT32:
+        strcpy(dst, "nr_uint32");
+        break;
+    case NR_INT64:
+        strcpy(dst, "nr_int64");
+        break;
+    case NR_UINT64:
+        strcpy(dst, "nr_uint64");
+        break;
+    case NR_FLOAT32:
+        strcpy(dst, "nr_float32");
+        break;
+    case NR_FLOAT64:
+        strcpy(dst, "nr_float64");
+        break;
+    default:
+        strcpy(dst, "UNKNOWN");
+        break;
+    }
+}
+
+NR_HEADER void
+NDtype_AsStringOnlyType(NR_DTYPE dtype, char dst[]){
+    switch (dtype)
+    {
+    case NR_BOOL:
+        strcpy(dst, "bool");
+        break;
+    case NR_INT8:
+        strcpy(dst, "int8");
+        break;
+    case NR_UINT8:
+        strcpy(dst, "uint8");
+        break;
+    case NR_INT16:
+        strcpy(dst, "int16");
+        break;
+    case NR_UINT16:
+        strcpy(dst, "uint16");
+        break;
+    case NR_INT32:
+        strcpy(dst, "int32");
+        break;
+    case NR_UINT32:
+        strcpy(dst, "uint32");
+        break;
+    case NR_INT64:
+        strcpy(dst, "int64");
+        break;
+    case NR_UINT64:
+        strcpy(dst, "uint64");
+        break;
+    case NR_FLOAT32:
+        strcpy(dst, "float32");
+        break;
+    case NR_FLOAT64:
+        strcpy(dst, "float64");
+        break;
+    default:
+        strcpy(dst, "UNKNOWN");
+        break;
+    }
+}
+
 
 #endif
