bf827b2cf9f2fba9f01e5acbfc0cff63be2cb7c2
= <=>
2024-11-24 11:50:04 +0300

    added NTools_BroadcastDtypes function

--- nour/_core/src/nour/ntools.h
+++ nour/_core/src/nour/ntools.h
@@ -8,4 +8,20 @@ NTools_BroadcastStrides(nr_size_t* a_shape, int a_ndim,
                         nr_size_t* a_strides, nr_size_t* src_shape,
                         int src_ndim, nr_size_t* out_strides);
 
+NR_STATIC_INLINE NR_DTYPE
+NTools_BroadcastDtypes(NR_DTYPE a, NR_DTYPE b){
+    if (a == b){
+        return a;
+    }
+
+    NR_DTYPE c = a > b ? a : b;
+    if (c <= NR_UINT64){
+        if ((c & 1) == 0){
+            return NR_FLOAT64;
+        }
+        return c;
+    }
+    return c;
+}
+
 #endif
