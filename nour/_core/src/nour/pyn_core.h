bc8f8aac57ce34e94454d22e69fabdaf64a2d7e4
= <normoslem256@gmail.com>
2024-12-02 22:53:53 +0300

    added PyNode fileds macros

--- nour/_core/src/nour/pyn_core.h
+++ nour/_core/src/nour/pyn_core.h
@@ -19,4 +19,14 @@ PyNode_Create(Node* node);
 NR_PUBLIC void
 PyNode_Delete(PyNode* self);
 
+#define PyNode_NDIM(pyn) (((PyNode*)pyn)->node->ndim)
+#define PyNode_SHAPE(pyn) (((PyNode*)pyn)->node->shape)
+#define PyNode_STRIDES(pyn) (((PyNode*)pyn)->node->strides)
+#define PyNode_DATA(pyn) (((PyNode*)pyn)->node->data)
+#define PyNode_FLAGS(pyn) (((PyNode*)pyn)->node->flags)
+#define PyNode_ITEMSIZE(pyn) (((PyNode*)pyn)->node->dtype.size)
+#define PyNode_NRDTYPE(pyn) (((PyNode*)pyn)->node->dtype.dtype)
+#define PyNode_DTYPE(pyn) (((PyNode*)pyn)->node->dtype)
+#define PyNode_BASE(pyn) (((PyNode*)pyn)->node->base)
+
 #endif
