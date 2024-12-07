5802f9c0ff41d7c33bc0a93bd1ba1c2d5f1e57ef
= <normoslem256@gmail.com>
2024-12-08 01:10:51 +0300

    fix(pyn_core): update PyNode_Create function signature and improve memory error handling
    - Changed the signature of PyNode_Create to accept a PyTypeObject pointer alongside the Node pointer.
    - Enhanced memory allocation error handling by using PyErr_NoMemory for clearer error reporting.
    - Removed deprecated code related to previous function signature for cleaner implementation.

--- nour/_core/src/nour/pyn_core.c
+++ nour/_core/src/nour/pyn_core.c
@@ -1,14 +1,12 @@
 #include "pyn_core.h"
 #include "node_core.h"
+#include <stdlib.h>
 
 NR_PUBLIC PyNode*
-PyNode_Create(Node* node){
-    PyNode* pyn = PyObject_New(PyNode, &PyNodeType);
+PyNode_Create(PyTypeObject* type_obj, Node* node){
+    PyNode* pyn = PyObject_New(PyNode, type_obj);
     if (pyn == NULL) {
-        PyErr_Format(
-            PyExc_MemoryError,
-            "can not allocate memory to create node object"
-        );
+        PyErr_NoMemory();
         return NULL;
     }
     pyn->node = node;
