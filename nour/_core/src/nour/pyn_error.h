01c7470e5b2addbd83f673e79626cbddacd26988
= <normoslem256@gmail.com>
2024-12-11 12:27:20 +0300

    feat(pyn_error): include nour/nour.h header

--- nour/_core/src/nour/pyn_error.h
+++ nour/_core/src/nour/pyn_error.h
@@ -1,13 +1,15 @@
 #ifndef NOUR__CORE_SRC_NOUR_PYN_ERROR_H
 #define NOUR__CORE_SRC_NOUR_PYN_ERROR_H
 
+#include "nour/nour.h"
+
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
 /**
  * Converts the current NError to a Python exception and raises it.
  */
-NR_PUBLIC void
+NR_PUBLIC void 
 NError_ToPyError();
 
 #endif
